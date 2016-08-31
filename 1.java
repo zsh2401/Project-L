c588cb0af55bfaec45e4904fddafc3b1d3784838
zsh2401 <zsh2401@163.com>
2016-08-31 10:07:37 +0800

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,16 @@
+class  helloworld
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		System.out.println("Hello World!too");
+	}
+}
+class BianLiang
+{
+	public static void main(String[]args)
+	{//什么时候定义变量？当数据不确定的时候，需要对数据进行存储的时候
+	 int x=4;	 //定义一个INT变量，取值为4
+	 System.out.println(x);//输出X的值
+	}
+}      
